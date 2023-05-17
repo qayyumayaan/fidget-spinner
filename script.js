64ca72cf0957b4ecf51f4c8bc63aed8d39fbcf96
qayyumayaan <96453808+qayyumayaan@users.noreply.github.com>
2023-05-17 19:18:05 -0400

    Exponentially slowing down

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
-var spinner = document.getElementById('spinner');
-var blades = document.getElementsByClassName('blade');
-var angle = 0;
-var speed = 0;
-
-function spin() {
-    angle += speed;
-    spinner.style.transform = 'rotate(' + angle + 'deg)';
-    requestAnimationFrame(spin);
-}
-
-function setSpeed(newSpeed) {
-    speed = newSpeed;
-}
-
-// Increase speed on mouse down
-document.addEventListener('mousedown', function() {
-    setSpeed(10);
-});
-
-// Decrease speed on mouse up
-document.addEventListener('mouseup', function() {
-    setSpeed(0);
-});
-
-// Stop spinning when mouse leaves the spinner
-spinner.addEventListener('mouseleave', function() {
-    setSpeed(0);
-});
-
-// Start spinning on page load
-spin();
+window.addEventListener('DOMContentLoaded', (event) => {
+    var spinner = document.getElementById('spinner');
+    var blades = document.getElementsByClassName('blade');
+    var angle = 0;
+    var speed = 0;
+    var momentum = 0.98; // Adjust this value to control the slowdown rate
+  
+    function spin() {
+      angle += speed;
+      spinner.style.transform = 'rotate(' + angle + 'deg)';
+  
+      // Apply momentum to slow down the spinner
+      speed *= momentum;
+  
+      // Stop spinning when the speed is very low
+      if (Math.abs(speed) < 0.1) {
+        setSpeed(0);
+      } else {
+        requestAnimationFrame(spin);
+      }
+    }
+  
+    function setSpeed(newSpeed) {
+      speed = newSpeed;
+    }
+  
+    // Increase speed on mouse down
+    document.addEventListener('mousedown', function () {
+      setSpeed(10);
+      spin(); // Start spinning immediately
+    });
+  
+    // Decrease speed on mouse up
+    document.addEventListener('mouseup', function () {
+      setSpeed(speed * momentum); // Apply momentum to the speed
+    });
+  
+    // Stop spinning when mouse leaves the spinner
+    spinner.addEventListener('mouseleave', function () {
+      setSpeed(speed * momentum); // Apply momentum to the speed
+    });
+  
+    // Start spinning on page load
+    spin();
+  });
+  
