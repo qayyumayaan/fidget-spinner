93fbe214fe37bc249fd7a51905d6a4ff678adb00
qayyumayaan <96453808+qayyumayaan@users.noreply.github.com>
2023-05-17 21:56:45 -0400

    Whitespace and prevents null in script.js

--- script.js
+++ script.js
@@ -1,58 +1,64 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    var spinner = document.getElementById('fidget-spinner'); // Change 'spinner' to 'fidget-spinner'
-    var blades = document.getElementsByClassName('blade');
-    var angle = 0;
-    var speed = 20;
-    var maxSpeed = 10;
-    var momentum = .9999;
-  
-    function spin() {
-      angle += speed;
-      spinner.style.transform = 'rotate(' + angle + 'deg)';
-  
-      // Apply momentum to slow down the spinner
-      speed *= momentum;
-  
-      // Stop spinning when the speed is very low
-      if (Math.abs(speed) < 0.05) {
-        setSpeed(0);
-      } else {
-        requestAnimationFrame(spin);
-      }
-    }
-  
-    function setSpeed(newSpeed) {
-      speed = newSpeed;
-    }
-  
-    // Increase speed on mouse down
-    document.addEventListener('mousedown', function () {
-      setSpeed(maxSpeed);
-      spin(); // Start spinning immediately
-    });
-  
-    // Decrease speed on mouse up
-    document.addEventListener('mouseup', function () {
-      setSpeed(speed * momentum); // Apply momentum to the speed
-    });
-  
-    // Stop spinning when mouse leaves the spinner
-    spinner.addEventListener('mouseleave', function () {
-      setSpeed(speed * momentum); // Apply momentum to the speed
-    });
-  
-    // Update momentum when the slider value changes
-    var momentumSlider = document.getElementById('momentum-slider');
-    momentumSlider.addEventListener('input', function () {
-      momentum = parseFloat(momentumSlider.value);
-    });
-  
-    // Update initial speed when the slider value changes
-    var speedSlider = document.getElementById('speed-slider');
-    speedSlider.addEventListener('input', function () {
-        maxSpeed = parseFloat(speedSlider.value);
-    });
+    var spinner = document.getElementById('fidget-spinner');
     
-    // Start spinning on page load
-    spin();
-});
+    if (spinner) { // Check if the spinner element is found
+      var blades = document.getElementsByClassName('blade');
+      var angle = 0;
+      var speed = 20;
+      var maxSpeed = 10;
+      var momentum = 0.9999;
+  
+      function spin() {
+        angle += speed;
+        spinner.style.transform = 'rotate(' + angle + 'deg)';
+  
+        // Apply momentum to slow down the spinner
+        speed *= momentum;
+  
+        // Stop spinning when the speed is very low
+        if (Math.abs(speed) < 0.05) {
+          setSpeed(0);
+        } else {
+          requestAnimationFrame(spin);
+        }
+      }
+  
+      function setSpeed(newSpeed) {
+        speed = newSpeed;
+      }
+  
+      // Increase speed on mouse down
+      document.addEventListener('mousedown', function () {
+        setSpeed(maxSpeed);
+        spin(); // Start spinning immediately
+      });
+  
+      // Decrease speed on mouse up
+      document.addEventListener('mouseup', function () {
+        setSpeed(speed * momentum); // Apply momentum to the speed
+      });
+  
+      // Stop spinning when mouse leaves the spinner
+      spinner.addEventListener('mouseleave', function () {
+        setSpeed(speed * momentum); // Apply momentum to the speed
+      });
+  
+      // Update momentum when the slider value changes
+      var momentumSlider = document.getElementById('momentum-slider');
+      momentumSlider.addEventListener('input', function () {
+        momentum = parseFloat(momentumSlider.value);
+      });
+  
+      // Update initial speed when the slider value changes
+      var speedSlider = document.getElementById('speed-slider');
+      speedSlider.addEventListener('input', function () {
+        maxSpeed = parseFloat(speedSlider.value);
+      });
+  
+      // Start spinning on page load
+      spin();
+    } else {
+      console.log("Spinner element not found.");
+    }
+  });
+  
